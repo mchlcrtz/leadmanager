cb3ec88751964556c375195e00982f108adad2ce
Michael Cortez <michael.cortez@analyticpartners.com>
2019-12-02 16:26:56 -0500

    add register and login components

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,5 +1,11 @@
 import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
 import { Provider as AlertProvider } from "react-alert";
@@ -21,12 +27,16 @@ class App extends Component {
     return (
       <Provider store={store}>
         <AlertProvider template={AlertTemplate} {...alertOptions}>
-          <Fragment>
-            <Header />
-            <div className="container">
-              <Dashboard />
-            </div>
-          </Fragment>
+          <Router>
+            <Fragment>
+              <Header />
+              <div className="container">
+                <Switch>
+                  <Route exact path="/" component={Dashboard} />
+                </Switch>
+              </div>
+            </Fragment>
+          </Router>
         </AlertProvider>
       </Provider>
     );
