a255b37ef826537b421f62c90d53c92054978660
Michael Cortez <michael.cortez@analyticpartners.com>
2019-12-02 11:22:25 -0500

    Add Alert component, type, and redcuer

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -2,8 +2,15 @@ import React, { Component, Fragment } from "react";
 import ReactDOM from "react-dom";
 import Header from "./layout/Header";
 import Dashboard from "./leads/Dashboard";
+import { Provider as AlertProvider } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 import { Provider } from "react-redux";
 import store from "../store";
+
+const alertOptions = {
+  timeout: 3000,
+  position: "top center"
+};
 
 class App extends Component {
   constructor(props) {
@@ -13,12 +20,14 @@ class App extends Component {
   render() {
     return (
       <Provider store={store}>
-        <Fragment>
-          <Header />
-          <div className="container">
-            <Dashboard />
-          </div>
-        </Fragment>
+        <AlertProvider template={AlertTemplate} {...alertOptions}>
+          <Fragment>
+            <Header />
+            <div className="container">
+              <Dashboard />
+            </div>
+          </Fragment>
+        </AlertProvider>
       </Provider>
     );
   }
