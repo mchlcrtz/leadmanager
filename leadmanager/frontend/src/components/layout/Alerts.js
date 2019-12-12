90a020d78c37952e5521fb9142e55e0b8fc019ae
Michael Cortez <mijcortez@gmail.com>
2019-12-12 16:53:26 -0500

    convert Alert component into sfc

--- leadmanager/frontend/src/components/layout/Alerts.js
+++ leadmanager/frontend/src/components/layout/Alerts.js
@@ -1,11 +1,17 @@
-import React, { Component, Fragment } from "react";
+import React, { Fragment, useEffect, useRef } from "react";
 import { withAlert } from "react-alert";
 import { connect } from "react-redux";
 
-export class Alerts extends Component {
-  componentDidUpdate(prevProps) {
-    const { error, alert, message } = this.props;
-    if (error !== prevProps.error) {
+export const Alerts = function Alerts(props) {
+  
+  const { error, alert, message } = props;
+  const prevErrorRef = useRef();
+  const prevMessageRef = useRef();
+
+  useEffect(() => {
+    prevErrorRef.current = error;
+    prevMessageRef.current = message;
+    if (error !== prevErrorRef.current) {
       if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
       if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
       if (error.msg.message)
@@ -15,17 +21,15 @@ export class Alerts extends Component {
       if (error.msg.username) alert.error(error.msg.username.join());
     }
 
-    if (message !== prevProps.message) {
+    if (message !== prevMessageRef.current) {
       if (message.deleteLead) alert.success(message.deleteLead);
       if (message.addLead) alert.success(message.addLead);
       if (message.passwordNotMatch) alert.error(message.passwordNotMatch);
     }
-  }
+  });
 
-  render() {
-    return <Fragment />;
-  }
-}
+  return <Fragment />;
+};
 
 const mapStateToProps = state => ({
   error: state.errors,
